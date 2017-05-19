c3965fe39a430ace88fc2ce9d10ffaf56044f6bd
Andrey Zak <azak@corp.finam.ru>
2017-05-19 20:37:55 +0300

    Call macros moved to nimbscript.nim

--- tasks/buildtools.nim
+++ tasks/buildtools.nim
@@ -1,10 +1,6 @@
 const
   msbuildExe* = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
 
-
-template call*(name: untyped) =   
-  `name Task`()
-
 proc toSln*(filename: string): string =
   filename & ".sln"
 
---
+++ tasks/nimbscript.nim
@@ -0,0 +1,2 @@
+template call*(name: untyped) =   
+  `name Task`()
