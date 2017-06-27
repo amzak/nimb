e0647e2f6d74d0b46da7f9fb7720fc197b6ff64c
Andrey Zak <azak@corp.finam.ru>
2017-06-27 20:28:11 +0300

    Added msbuild version selection

--- tasks/msbuild.nim
+++ tasks/msbuild.nim
@@ -2,11 +2,16 @@ const
   msbuildExe14 = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
   msbuildExe15 = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\\MSBuild\\15.0\\Bin\\MSBuild.exe"
 
-proc msbuildExe*(): string =
-  if fileExists(msbuildExe15):
-    result = msbuildExe15
-  else:
-    result = msbuildExe14
+type
+  msbuildVersion* = enum
+    v14, v15 
+
+proc msbuildExe*(version: msbuildVersion): string =
+  case version:
+    of v14:
+      result = msbuildExe14
+    of v15:
+      result = msbuildExe15  
 
 template msbuild*(body: untyped) = 
   proc `msbuild Task`*() = 
