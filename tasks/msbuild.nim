1f2f16a81c5e55c722a6cd08109e2a0f50bd98a2
Andrey Zak <azak@corp.finam.ru>
2017-06-27 13:57:57 +0300

    Detect MSBuild in new place, hardcoded

--- tasks/msbuild.nim
+++ tasks/msbuild.nim
@@ -1,5 +1,12 @@
 const
-  msbuildExe = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
+  msbuildExe14 = "C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe"
+  msbuildExe15 = "C:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Professional\\MSBuild\\15.0\\Bin\\MSBuild.exe"
+
+proc msbuildExe*(): string =
+  if fileExists(msbuildExe15):
+    result = msbuildExe15
+  else:
+    result = msbuildExe14
 
 template msbuild*(body: untyped) = 
   proc `msbuild Task`*() = 
